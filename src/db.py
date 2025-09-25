1597a11b15d680f3d2167937f27100484543ff0a
tarun851 <tarunkumar271205@gmail.com>
2025-09-25 11:45:04 +0530

    db.py completed

---
+++ src/db.py
@@ -0,0 +1,35 @@
+#db.py
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+#load environmeent variables
+load_dotenv()
+url=os.getenv("SUPABASE_URL")
+key=os.getenv("SUPABASE_KEY")
+
+supabase=create_client(url,key)
+
+#create Veihcles table
+def create_Vehicles(vehicle_number,vehicle_type ,manufacturer,year_of_manufacture ,date_of_registration):
+    return supabase.table("Vehicles").insert({
+        "vehicle_number":vehicle_number,
+        "vehicle_type":vehicle_type,
+        "manufacturer":manufacturer,
+        "year_of_manufacture":year_of_manufacture,
+        "date_of_registration":date_of_registration,
+        "Completed":False
+    }).execute()
+
+#get all tasks
+def get__all_Vehicles():
+    return supabase.table("Vehicles").select("*").order("date_of_registration").execute()
+
+#update tasks status
+def update_Vehicles(vehicle_id,manufacturer):
+    return supabase.table("Vehicles").update({"manufacturer":manufacturer}).eq("vehicle_id",vehicle_id).execute()
+
+#delete task
+def delete_Vehicles(vehicle_id):
+    return supabase.table("Vehicles").delete().eq("vehicle_id",vehicle_id).execute()
+
